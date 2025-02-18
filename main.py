4cb76c4f206ab26e74b8786327cad801bb2ec94c
Uncle-Sam137 <semen.kolesnik@yandex.ru>
2025-02-18 14:08:57 +0300

    Добавил проверки: на код состояния, на преобразование в JSON, на то, что API вернул именно словари, а не список, число или строку

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 
@@ -7,8 +9,25 @@ def bm_test_zadanie():
     которые появились во втором запросе, но отсутствовали в первом.
     """
     # Делаем два запроса к API
-    first_response = requests.get("https://base.media108.ru/training/sample/").json()
-    second_response = requests.get("https://base.media108.ru/training/sample/").json()
+    first_response = requests.get("https://base.media108.ru/training/sample/", timeout=5)
+    second_response = requests.get("https://base.media108.ru/training/sample/", timeout=5)
+
+    # Проверяем, что сервер вернул 200 для обоих запросов
+    if first_response.status_code != 200 or second_response.status_code != 200:
+        return (f'Ошибка API: '
+                f'\nКод первого запроса: {first_response.status_code}'
+                f'\nКод второго запроса: {second_response.status_code}')
+
+    # Пробуем преобразовать ответы в JSON, обрабатываем возможную ошибку
+    try:
+        first_response = first_response.json()
+        second_response = second_response.json()
+    except json.decoder.JSONDecodeError:
+        return f"Ошибка API: формат ответа не JSON"
+
+    # Проверяем, что API вернул именно словари, а не список, число или строку
+    if not isinstance(first_response, dict) or not isinstance(second_response, dict):
+        return f"Ошибка API: формат ответа не словарь JSON"
 
     res = dict()
 
